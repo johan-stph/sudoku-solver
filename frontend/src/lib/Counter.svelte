53681c0bcfde3dee2251e75bb9e05acdd1d36aa1
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-09-30 10:51:39 +0200

    added tailwind.css

---
+++ frontend/src/lib/Counter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let count = 0;
+    function increment() {
+        count += 1
+    }
+</script>
+
+<button on:click={increment}> Click me </button>
+<p>{count}</p>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Counter from "$lib/Counter.svelte";
+</script>
+
+<main>
+    <h1 class="text-2xl bg-amber-200">Home Page</h1>
+    <Counter />
+</main>
