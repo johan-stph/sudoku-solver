879ecec42b813bf7b497507b140558ff457da783
Johannes Stephan <111685646+johan-stph@users.noreply.github.com>
2023-10-01 22:39:10 +0200

    Frontend Scaffold (#3)
    * init scaffold svelte
    
    * added tailwind.css
    
    * added sudoku layout
    
    * added frontend without logic

---
+++ frontend/src/lib/SudokuGrid.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    export let board: number[][];
+
+    let canvas: HTMLCanvasElement;
+    let selectedCell: { row: number, col: number } | null = null;
+
+    onMount(() => {
+        window.addEventListener('keydown', handleKeydown);
+        return () => {
+            window.removeEventListener('keydown', handleKeydown);
+        };
+    });
+
+    function handleClick(event: MouseEvent) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        const cellSize = 50; // Size of each cell
+
+        const row = Math.floor(y / cellSize);
+        const col = Math.floor(x / cellSize);
+
+        selectedCell = {row, col};
+    }
+
+    function handleKeydown(event: KeyboardEvent) {
+        if (selectedCell) {
+            const value = parseInt(event.key);
+            if (value >= 1 && value <= 9) {
+                board[selectedCell.row][selectedCell.col] = value;
+            }
+        }
+    }
+
+    // Reactive statement to re-draw the canvas whenever the board changes
+    $: {
+        if (canvas) {  // Check if canvas is defined
+            const dpr = window.devicePixelRatio || 1;
+            const rect = canvas.getBoundingClientRect();
+
+            // Multiply canvas dimensions by device pixel ratio
+            canvas.width = rect.width * dpr;
+            canvas.height = rect.height * dpr;
+
+            const ctx = canvas.getContext('2d')!;
+
+            // Scale the drawing context by the device pixel ratio
+            ctx.scale(dpr, dpr);
+
+            const cellSize = 50; // Size of each cell
+            const boardSize = 9 * cellSize;
+            const thickLine = 4;
+            const thinLine = 1;
+
+            // Clear the canvas before redrawing
+            ctx.clearRect(0, 0, boardSize, boardSize);
+
+            // Draw grid
+            for (let i = 0; i <= 9; i++) {
+                ctx.lineWidth = i % 3 === 0 ? thickLine : thinLine;
+
+                // Vertical lines
+                ctx.beginPath();
+                ctx.moveTo(i * cellSize, 0);
+                ctx.lineTo(i * cellSize, boardSize);
+                ctx.stroke();
+
+                // Horizontal lines
+                ctx.beginPath();
+                ctx.moveTo(0, i * cellSize);
+                ctx.lineTo(boardSize, i * cellSize);
+                ctx.stroke();
+            }
+
+            // Draw numbers
+            ctx.font = '36px Arial';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            for (let row = 0; row < 9; row++) {
+                for (let col = 0; col < 9; col++) {
+                    const value = board[row][col];
+                    if (value !== 0) {
+                        ctx.fillText(value.toString(), col * cellSize + cellSize / 2, row * cellSize + cellSize / 2);
+                    }
+                }
+            }
+            if (selectedCell) {
+                ctx.fillStyle = 'rgba(0, 128, 255, 0.3)';  // Semi-transparent blue
+                ctx.fillRect(
+                    selectedCell.col * cellSize,
+                    selectedCell.row * cellSize,
+                    cellSize,
+                    cellSize
+                );
+            }
+        }
+    }
+</script>
+
+<div class="game-wrapper">
+    <div id="game" class="game">
+        <canvas bind:this={canvas} width="450" height="450" style="width: 450px; height: 450px;"
+                on:click={handleClick}></canvas>
+    </div>
+</div>
+
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Sudoku from "$lib/Sudoku.svelte";
+</script>
+
+<main>
+    <h1 class="flex justify-center text-2xl">Sudoku Solver</h1>
+    <Sudoku />
+</main>
